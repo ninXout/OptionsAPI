72f96bcfa595dc8f9257627bcce42d60a3b2982b
Erymanthus (RayDeeUx) <raydeeux@gmail.com>
2026-02-19 14:51:05 -0500

    Port to Geode v5

--- include/API.hpp
+++ include/API.hpp
@@ -16,16 +16,16 @@ using EditInitialCallback = std::function<bool()>;
 #define READONLY(mem, name, type) type get##name() const { return mem; };
 
 #define TOGGLEEVENT(evname, toggle, call) \
-class Add##evname##ToggleEvent : public Event { \
-protected: \
+class Add##evname##ToggleEvent : public Event<Add##evname##ToggleEvent, bool(std::string_view name, std::string_view id, toggle callback, call initialValue, std::string_view desc)> { \
+public: \
+    using Event::Event; \
     std::string m_name; \
     std::string m_id; \
     toggle m_callback; \
     call m_initialValue; \
     std::string m_description; \
  \
-public: \
-    Add##evname##ToggleEvent(std::string_view name, std::string_view id,  toggle callback,  call initialValue, std::string_view desc) : m_name(name), m_id(id), m_callback(callback), m_initialValue(initialValue), m_description(desc) {}; \
+    Add##evname##ToggleEvent(std::string_view name, std::string_view id, toggle callback, call initialValue, std::string_view desc) : m_name(name), m_id(id), m_callback(callback), m_initialValue(initialValue), m_description(desc) {}; \
  \
     READONLY(m_name, Name, std::string); \
     READONLY(m_id, ID, std::string); \
@@ -50,16 +50,16 @@ namespace OptionsAPI { // TODO: expand to float, int, and std::string
 
     template <>
     void addPreLevelSetting<bool>(std::string_view name, std::string_view id, PreToggleCallback callback, PreInitialCallback initialValue, std::string_view desc) {
-        AddPreToggleEvent(name, id, callback, initialValue, desc).post();
+        AddPreToggleEvent().send(name, id, callback, initialValue, desc);
     }
 
     template <>
     void addMidLevelSetting<bool>(std::string_view name, std::string_view id, MidToggleCallback callback, MidInitialCallback initialValue, std::string_view desc) {
-        AddMidToggleEvent(name, id, callback, initialValue, desc).post();
+        AddMidToggleEvent().send(name, id, callback, initialValue, desc);
     }
 
     template <>
     void addEditorLevelSetting<bool>(std::string_view name, std::string_view id, EditToggleCallback callback, EditInitialCallback initialValue, std::string_view desc) {
-        AddEditToggleEvent(name, id, callback, initialValue, desc).post();
+        AddEditToggleEvent().send(name, id, callback, initialValue, desc);
     }
 }
