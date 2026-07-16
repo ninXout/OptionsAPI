0deb58803ae60f9d2b006c894a4cb6f85b3a73be
Erymanthus (RayDeeUx) <raydeeux@gmail.com>
2026-07-16 11:53:52 -0400

    ????????????

--- src/main.cpp
+++ src/main.cpp
@@ -85,7 +85,8 @@ $execute {
 #define PRE_TOGGLES_START 5
 #endif
 #define MID_TOGGLES_START 12
-#define EDIT_TOGGLES_START 26
+// so the tags in editoroptionslayer are all sorts of fucked
+#define EDIT_TOGGLES_START 200
 
 #include <Geode/modify/GameLevelOptionsLayer.hpp>
 class $modify(GameLevelOptionsLayer) {
@@ -472,15 +473,7 @@ class $modify(EditorOptionsLayer) {
 	}
 
 	void didToggle(int opt) {
-		switch (opt) {
-			case 1: case 2: case 3: case 4: case 5:
-			case 6: case 7: case 8: case 9: case 10:
-			case 11: case 12: case 13: case 14: case 15:
-			case 16: case 17: case 18: case 19: case 20:
-			case 21: case 22: case 23: case 24: case 25: // highkey i think rob only added 23 toggles in 2.2082, but anything for consistency, i guess... --raydeeux
-				return EditorOptionsLayer::didToggle(opt);
-			default:
-				return std::next(g_editToggles.begin(), opt - EDIT_TOGGLES_START)->second.m_callback(); // this might be stupid idk
-		}
+		if (opt > EDIT_TOGGLES_START - 1) return std::next(g_editToggles.begin(), opt - EDIT_TOGGLES_START)->second.m_callback(); // this might be stupid idk
+		EditorOptionsLayer::didToggle(opt);
 	}
 };
